f0b58bec66f1dd1d41bf563c16b9b7276307d860
Priti Maurya <maurya.priti032001@gmail.com>
2024-07-09 14:14:45 +0530

    Update detect_text.py

--- detect_text.py
+++ detect_text.py
@@ -4,7 +4,7 @@ import os
 from playsound import playsound
 
 # Set the path to the Tesseract executable (if necessary)
-pytesseract.pytesseract.tesseract_cmd = r'C://Program Files//Tesseract-OCR//tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'your Tesseract path'
 
 # Set the TESSDATA_PREFIX environment variable
 os.environ['TESSDATA_PREFIX'] = r'C://Program Files//Tesseract-OCR//tessdata'
